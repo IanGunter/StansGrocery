934c1e381788d57f0c7f2498d38be971d8eed5e5
Ian Gunter <iangunterofamerica@yahoo.com>
2020-10-28 17:45:36 -0600

    Add project files.

---
+++ StansGroceryForm.vb
@@ -0,0 +1,17 @@
+﻿'Ian Gunter
+'RCET 0265
+'StansGrocery
+'Fall 2020
+'
+
+
+
+
+Public Class StansGroceryForm
+    Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
+        If LookUpTextBox.Text = "zzz" Then
+            Me.Close()
+        End If
+
+    End Sub
+End Class
