b1bbe86afc76a3c7d98cf72fa2c1b6c45fda4350
Ian Gunter <iangunterofamerica@yahoo.com>
2020-11-07 20:35:31 -0700

    Change Arrays

--- StansGroceryForm.vb
+++ StansGroceryForm.vb
@@ -7,21 +7,43 @@
 Option Strict On
 Option Compare Text
 Option Explicit On
-
+Imports System.Text.RegularExpressions
 
 Public Class StansGroceryForm
 
-    Dim tempdata() As String
+    Dim lineData() As String
+    Dim SeperatedData() As String
+    Dim betterSepArray() As String
+    Dim FoodArray(0, 2) As String
     Dim i As Integer
+    Dim u As Integer
     Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
         If LookUpTextBox.Text = "zzz" Then
             Me.Close()
         End If
 
-        tempdata = Split(My.Resources.GroceryList, vbNewLine)
+        lineData = Split(My.Resources.GroceryList, vbNewLine)
 
-        MsgBox(tempdata(i))
-        i += 1
+        For Each thingy In lineData
+
+            SeperatedData = Split(thingy, $"{Chr(34)},{Chr(34)}")
+
+            betterSepArray = Split(SeperatedData(0), "$$ITM")
+            FoodArray(i, u) = betterSepArray(1)
+            u += 1
+            betterSepArray = Split(SeperatedData(1), "##LOC")
+            FoodArray(i, u) = betterSepArray(1)
+            u += 1
+            betterSepArray = Split(SeperatedData(2), "%%CAT")
+            FoodArray(i, u) = betterSepArray(1)
+            u = 0
+            i += 1
+            ReDim Preserve FoodArray
+            'betterSepArray = Split(betterSepArray(1), Chr(34))
+        Next
+
+
+
 
 
 
