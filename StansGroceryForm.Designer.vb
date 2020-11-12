e710107dca7568e85da43d46464f89a05175fe84
Ian Gunter <iangunterofamerica@yahoo.com>
2020-11-11 21:12:42 -0700

    Add Arrays Add Search function Add About Form Add Fun

--- StansGroceryForm.Designer.vb
+++ StansGroceryForm.Designer.vb
@@ -22,11 +22,14 @@ Partial Class StansGroceryForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.TopMenuStrip = New System.Windows.Forms.MenuStrip()
         Me.FileTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SearchToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.HelpTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.AboutTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.LookUpTextBox = New System.Windows.Forms.TextBox()
+        Me.searchTextBox = New System.Windows.Forms.TextBox()
         Me.SelectItemComboBox = New System.Windows.Forms.ComboBox()
         Me.DisplayListBox = New System.Windows.Forms.ListBox()
         Me.LookUpLabel = New System.Windows.Forms.Label()
@@ -34,10 +37,10 @@ Partial Class StansGroceryForm
         Me.SearchButton = New System.Windows.Forms.Button()
         Me.LeftGroupBox = New System.Windows.Forms.GroupBox()
         Me.DisplayLabel = New System.Windows.Forms.Label()
-        Me.AllRadioButton = New System.Windows.Forms.RadioButton()
         Me.CategoryButton = New System.Windows.Forms.RadioButton()
         Me.AisleRadioButton = New System.Windows.Forms.RadioButton()
         Me.FilterGroupBox = New System.Windows.Forms.GroupBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
         Me.TopMenuStrip.SuspendLayout()
         Me.LeftGroupBox.SuspendLayout()
         Me.FilterGroupBox.SuspendLayout()
@@ -45,155 +48,157 @@ Partial Class StansGroceryForm
         '
         'TopMenuStrip
         '
-        Me.TopMenuStrip.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
         Me.TopMenuStrip.ImageScalingSize = New System.Drawing.Size(32, 32)
         Me.TopMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileTopMenuItem, Me.HelpTopMenuItem})
         Me.TopMenuStrip.Location = New System.Drawing.Point(0, 0)
         Me.TopMenuStrip.Name = "TopMenuStrip"
-        Me.TopMenuStrip.Size = New System.Drawing.Size(1250, 48)
+        Me.TopMenuStrip.Padding = New System.Windows.Forms.Padding(3, 1, 0, 1)
+        Me.TopMenuStrip.Size = New System.Drawing.Size(625, 24)
         Me.TopMenuStrip.TabIndex = 0
         Me.TopMenuStrip.Text = "MenuStrip1"
         '
         'FileTopMenuItem
         '
+        Me.FileTopMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SearchToolStripMenuItem, Me.ExitToolStripMenuItem})
         Me.FileTopMenuItem.Name = "FileTopMenuItem"
-        Me.FileTopMenuItem.Size = New System.Drawing.Size(72, 40)
+        Me.FileTopMenuItem.Size = New System.Drawing.Size(37, 22)
         Me.FileTopMenuItem.Text = "&File"
+        '
+        'SearchToolStripMenuItem
+        '
+        Me.SearchToolStripMenuItem.Name = "SearchToolStripMenuItem"
+        Me.SearchToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SearchToolStripMenuItem.Text = "Search"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
         '
         'HelpTopMenuItem
         '
         Me.HelpTopMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutTopMenuItem})
         Me.HelpTopMenuItem.Name = "HelpTopMenuItem"
-        Me.HelpTopMenuItem.Size = New System.Drawing.Size(85, 40)
+        Me.HelpTopMenuItem.Size = New System.Drawing.Size(44, 22)
         Me.HelpTopMenuItem.Text = "Help"
         '
         'AboutTopMenuItem
         '
         Me.AboutTopMenuItem.Name = "AboutTopMenuItem"
-        Me.AboutTopMenuItem.Size = New System.Drawing.Size(214, 44)
+        Me.AboutTopMenuItem.Size = New System.Drawing.Size(180, 22)
         Me.AboutTopMenuItem.Text = "About"
         '
-        'LookUpTextBox
+        'searchTextBox
         '
-        Me.LookUpTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append
-        Me.LookUpTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllSystemSources
-        Me.LookUpTextBox.Location = New System.Drawing.Point(164, 95)
-        Me.LookUpTextBox.Margin = New System.Windows.Forms.Padding(6)
-        Me.LookUpTextBox.Name = "LookUpTextBox"
-        Me.LookUpTextBox.Size = New System.Drawing.Size(818, 31)
-        Me.LookUpTextBox.TabIndex = 1
+        Me.searchTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append
+        Me.searchTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllSystemSources
+        Me.searchTextBox.Location = New System.Drawing.Point(82, 49)
+        Me.searchTextBox.Name = "searchTextBox"
+        Me.searchTextBox.Size = New System.Drawing.Size(411, 20)
+        Me.searchTextBox.TabIndex = 1
+        Me.ToolTip1.SetToolTip(Me.searchTextBox, "Search for an Item ")
         '
         'SelectItemComboBox
         '
         Me.SelectItemComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append
         Me.SelectItemComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
         Me.SelectItemComboBox.FormattingEnabled = True
-        Me.SelectItemComboBox.Location = New System.Drawing.Point(164, 138)
-        Me.SelectItemComboBox.Margin = New System.Windows.Forms.Padding(6)
+        Me.SelectItemComboBox.Location = New System.Drawing.Point(82, 72)
         Me.SelectItemComboBox.Name = "SelectItemComboBox"
-        Me.SelectItemComboBox.Size = New System.Drawing.Size(432, 33)
+        Me.SelectItemComboBox.Size = New System.Drawing.Size(218, 21)
         Me.SelectItemComboBox.TabIndex = 2
+        Me.ToolTip1.SetToolTip(Me.SelectItemComboBox, "Select the Aisle/ Category of the item needed")
         '
         'DisplayListBox
         '
         Me.DisplayListBox.FormattingEnabled = True
-        Me.DisplayListBox.ItemHeight = 25
-        Me.DisplayListBox.Location = New System.Drawing.Point(612, 138)
-        Me.DisplayListBox.Margin = New System.Windows.Forms.Padding(6)
+        Me.DisplayListBox.Location = New System.Drawing.Point(306, 72)
         Me.DisplayListBox.Name = "DisplayListBox"
-        Me.DisplayListBox.Size = New System.Drawing.Size(578, 479)
+        Me.DisplayListBox.Size = New System.Drawing.Size(291, 251)
         Me.DisplayListBox.TabIndex = 3
+        Me.ToolTip1.SetToolTip(Me.DisplayListBox, "Filtered Items appear here ")
         '
         'LookUpLabel
         '
         Me.LookUpLabel.AutoSize = True
-        Me.LookUpLabel.Location = New System.Drawing.Point(10, 88)
-        Me.LookUpLabel.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.LookUpLabel.Location = New System.Drawing.Point(5, 46)
         Me.LookUpLabel.Name = "LookUpLabel"
-        Me.LookUpLabel.Size = New System.Drawing.Size(138, 25)
+        Me.LookUpLabel.Size = New System.Drawing.Size(71, 13)
         Me.LookUpLabel.TabIndex = 4
         Me.LookUpLabel.Text = "Look Up Item"
         '
         'SelectLabel
         '
         Me.SelectLabel.AutoSize = True
-        Me.SelectLabel.Location = New System.Drawing.Point(32, 138)
-        Me.SelectLabel.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.SelectLabel.Location = New System.Drawing.Point(16, 72)
         Me.SelectLabel.Name = "SelectLabel"
-        Me.SelectLabel.Size = New System.Drawing.Size(118, 25)
+        Me.SelectLabel.Size = New System.Drawing.Size(60, 13)
         Me.SelectLabel.TabIndex = 5
         Me.SelectLabel.Text = "Select Item"
         '
         'SearchButton
         '
-        Me.SearchButton.Location = New System.Drawing.Point(998, 88)
-        Me.SearchButton.Margin = New System.Windows.Forms.Padding(6)
+        Me.SearchButton.Location = New System.Drawing.Point(499, 46)
         Me.SearchButton.Name = "SearchButton"
-        Me.SearchButton.Size = New System.Drawing.Size(196, 38)
+        Me.SearchButton.Size = New System.Drawing.Size(98, 20)
         Me.SearchButton.TabIndex = 6
         Me.SearchButton.Text = "&Search"
+        Me.ToolTip1.SetToolTip(Me.SearchButton, "Searches ")
         Me.SearchButton.UseVisualStyleBackColor = True
         '
         'LeftGroupBox
         '
         Me.LeftGroupBox.Controls.Add(Me.DisplayLabel)
-        Me.LeftGroupBox.Location = New System.Drawing.Point(164, 190)
-        Me.LeftGroupBox.Margin = New System.Windows.Forms.Padding(6)
+        Me.LeftGroupBox.Location = New System.Drawing.Point(82, 99)
         Me.LeftGroupBox.Name = "LeftGroupBox"
-        Me.LeftGroupBox.Padding = New System.Windows.Forms.Padding(6)
-        Me.LeftGroupBox.Size = New System.Drawing.Size(436, 431)
+        Me.LeftGroupBox.Size = New System.Drawing.Size(218, 224)
         Me.LeftGroupBox.TabIndex = 7
         Me.LeftGroupBox.TabStop = False
         '
         'DisplayLabel
         '
-        Me.DisplayLabel.Location = New System.Drawing.Point(6, 31)
-        Me.DisplayLabel.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.DisplayLabel.Location = New System.Drawing.Point(3, 16)
         Me.DisplayLabel.Name = "DisplayLabel"
-        Me.DisplayLabel.Size = New System.Drawing.Size(418, 394)
+        Me.DisplayLabel.Size = New System.Drawing.Size(209, 205)
         Me.DisplayLabel.TabIndex = 0
-        '
-        'AllRadioButton
-        '
-        Me.AllRadioButton.AutoSize = True
-        Me.AllRadioButton.Location = New System.Drawing.Point(12, 40)
-        Me.AllRadioButton.Name = "AllRadioButton"
-        Me.AllRadioButton.Size = New System.Drawing.Size(67, 29)
-        Me.AllRadioButton.TabIndex = 8
-        Me.AllRadioButton.TabStop = True
-        Me.AllRadioButton.Text = "All"
-        Me.AllRadioButton.UseVisualStyleBackColor = True
+        Me.ToolTip1.SetToolTip(Me.DisplayLabel, "Displays the item's Location and Category")
         '
         'CategoryButton
         '
         Me.CategoryButton.AutoSize = True
-        Me.CategoryButton.Location = New System.Drawing.Point(12, 75)
+        Me.CategoryButton.Location = New System.Drawing.Point(6, 19)
+        Me.CategoryButton.Margin = New System.Windows.Forms.Padding(2)
         Me.CategoryButton.Name = "CategoryButton"
-        Me.CategoryButton.Size = New System.Drawing.Size(130, 29)
+        Me.CategoryButton.Size = New System.Drawing.Size(67, 17)
         Me.CategoryButton.TabIndex = 9
         Me.CategoryButton.TabStop = True
         Me.CategoryButton.Text = "Category"
+        Me.ToolTip1.SetToolTip(Me.CategoryButton, "Filter by Category")
         Me.CategoryButton.UseVisualStyleBackColor = True
         '
         'AisleRadioButton
         '
         Me.AisleRadioButton.AutoSize = True
-        Me.AisleRadioButton.Location = New System.Drawing.Point(12, 110)
+        Me.AisleRadioButton.Location = New System.Drawing.Point(6, 37)
+        Me.AisleRadioButton.Margin = New System.Windows.Forms.Padding(2)
         Me.AisleRadioButton.Name = "AisleRadioButton"
-        Me.AisleRadioButton.Size = New System.Drawing.Size(90, 29)
+        Me.AisleRadioButton.Size = New System.Drawing.Size(47, 17)
         Me.AisleRadioButton.TabIndex = 10
         Me.AisleRadioButton.TabStop = True
         Me.AisleRadioButton.Text = "Aisle"
+        Me.ToolTip1.SetToolTip(Me.AisleRadioButton, "Filter by Aisle")
         Me.AisleRadioButton.UseVisualStyleBackColor = True
         '
         'FilterGroupBox
         '
         Me.FilterGroupBox.Controls.Add(Me.CategoryButton)
         Me.FilterGroupBox.Controls.Add(Me.AisleRadioButton)
-        Me.FilterGroupBox.Controls.Add(Me.AllRadioButton)
-        Me.FilterGroupBox.Location = New System.Drawing.Point(8, 190)
+        Me.FilterGroupBox.Location = New System.Drawing.Point(4, 99)
+        Me.FilterGroupBox.Margin = New System.Windows.Forms.Padding(2)
         Me.FilterGroupBox.Name = "FilterGroupBox"
-        Me.FilterGroupBox.Size = New System.Drawing.Size(148, 155)
+        Me.FilterGroupBox.Padding = New System.Windows.Forms.Padding(2)
+        Me.FilterGroupBox.Size = New System.Drawing.Size(74, 61)
         Me.FilterGroupBox.TabIndex = 11
         Me.FilterGroupBox.TabStop = False
         Me.FilterGroupBox.Text = "Filter By:"
@@ -201,9 +206,9 @@ Partial Class StansGroceryForm
         'StansGroceryForm
         '
         Me.AcceptButton = Me.SearchButton
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1250, 667)
+        Me.ClientSize = New System.Drawing.Size(625, 347)
         Me.Controls.Add(Me.FilterGroupBox)
         Me.Controls.Add(Me.LeftGroupBox)
         Me.Controls.Add(Me.SearchButton)
@@ -211,10 +216,9 @@ Partial Class StansGroceryForm
         Me.Controls.Add(Me.LookUpLabel)
         Me.Controls.Add(Me.DisplayListBox)
         Me.Controls.Add(Me.SelectItemComboBox)
-        Me.Controls.Add(Me.LookUpTextBox)
+        Me.Controls.Add(Me.searchTextBox)
         Me.Controls.Add(Me.TopMenuStrip)
         Me.MainMenuStrip = Me.TopMenuStrip
-        Me.Margin = New System.Windows.Forms.Padding(6)
         Me.Name = "StansGroceryForm"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Welcome to Stan's Grocery"
@@ -232,7 +236,7 @@ Partial Class StansGroceryForm
     Friend WithEvents FileTopMenuItem As ToolStripMenuItem
     Friend WithEvents HelpTopMenuItem As ToolStripMenuItem
     Friend WithEvents AboutTopMenuItem As ToolStripMenuItem
-    Friend WithEvents LookUpTextBox As TextBox
+    Friend WithEvents searchTextBox As TextBox
     Friend WithEvents SelectItemComboBox As ComboBox
     Friend WithEvents DisplayListBox As ListBox
     Friend WithEvents LookUpLabel As Label
@@ -240,8 +244,10 @@ Partial Class StansGroceryForm
     Friend WithEvents SearchButton As Button
     Friend WithEvents LeftGroupBox As GroupBox
     Friend WithEvents DisplayLabel As Label
-    Friend WithEvents AllRadioButton As RadioButton
     Friend WithEvents CategoryButton As RadioButton
     Friend WithEvents AisleRadioButton As RadioButton
     Friend WithEvents FilterGroupBox As GroupBox
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents SearchToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
 End Class
--- StansGroceryForm.vb
+++ StansGroceryForm.vb
@@ -11,27 +11,57 @@ Imports System.Text.RegularExpressions
 
 Public Class StansGroceryForm
     Dim lineData() As String
-    Dim SeperatedData() As String
+    Dim seperatedData() As String
     Dim betterSepArray() As String
+    Dim sortedAisles() As String
+    Dim sortedcategories() As String
+    Dim fullArray2(,) As String
+    Dim sizer As Integer
 
-    Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
+    Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click, SearchToolStripMenuItem.Click
 
-        If LookUpTextBox.Text = "zzz" Then
+        Dim matched As Match
+        Dim goodData As Boolean
+        goodData = False
+        'closes the program when ZZZ is searched
+        If searchTextBox.Text = "zzz" Then
             Me.Close()
         End If
 
+        DisplayListBox.Items.Clear()
+
+        'Display items in the displayListbox when they match a search.
+        For n = 0 To sizer
+            Try
+                matched = Regex.Match(fullArray2(n, 0), searchTextBox.Text, RegexOptions.IgnoreCase)
+            Catch ex As Exception
+            End Try
+
+            If matched.Success = True Then
+                goodData = True
+                DisplayListBox.Items.Add(fullArray2(n, 0))
+                DisplayLabel.Text = ""
+            Else
+            End If
+
+        Next
+        If goodData = False Then
+            DisplayLabel.Text = ("Sorry no matches for " & searchTextBox.Text)
+        End If
+
+
+
+
     End Sub
 
-    Private Sub StansGroceryForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Public Sub StansGroceryForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         SplashScreenForm.Show()
         Dim match As Match
-        Dim Sizer As Integer
         Dim i As Integer
         Dim u As Integer
 
 
-        AllRadioButton.Checked = True
         lineData = Split(My.Resources.GroceryList, vbNewLine)
 
         'Autosizer used to find how big an Array should be based off counting significant text matches.
@@ -39,40 +69,170 @@ Public Class StansGroceryForm
             match = Regex.Match(lineData(i), "ITM")
 
             If match.Success = True Then
-                Sizer += 1
+                sizer += 1
             End If
         Next
 
-        Dim fullArray(Sizer, 2) As String
+        Dim fullArray(sizer, 2) As String
 
+        'Series of arrays that sorts the Data in the text file.
         For Each thingy In lineData
             u = 0
-            SeperatedData = Split(thingy, $"{Chr(34)},{Chr(34)}")
-            betterSepArray = Split(SeperatedData(0), "$$ITM")
+            seperatedData = Split(thingy, $"{Chr(34)},{Chr(34)}")
+            betterSepArray = Split(seperatedData(0), "$$ITM")
             fullArray(i, u) = betterSepArray(1)
             u += 1
-            betterSepArray = Split(SeperatedData(1), "##LOC")
+            betterSepArray = Split(seperatedData(1), "##LOC")
             fullArray(i, u) = betterSepArray(1)
             u += 1
-            betterSepArray = Split(SeperatedData(2), "%%CAT")
+            betterSepArray = Split(seperatedData(2), "%%CAT")
             betterSepArray = Split(betterSepArray(1), Chr(34))
             fullArray(i, u) = betterSepArray(0)
 
-            If i <> Sizer Then
+            If i <> sizer Then
                 i += 1
             End If
         Next
 
-        For Variable = 0 To Sizer
-            Try
+        'Copy FullArray into FullArray 2 to make a global 
+        fullArray2 = fullArray
 
+        'Add Items to listBox
+        For Variable = 0 To sizer
+            Try
                 DisplayListBox.Items.Add(fullArray(Variable, 0))
             Catch Ex As Exception
-
             End Try
         Next
+
         DisplayListBox.Sorted = True
+        AisleSorter()
+        CatSorter()
+        CategoryButton.Checked = True
 
     End Sub
 
+    Sub AisleSorter()
+        'Sorts the ailse numbers in numerical order
+        Dim Ailes(sizer) As String
+
+        For a = 0 To sizer
+            Ailes(a) = fullArray2(a, 1)
+
+            Dim stuff As String = String.Join(",", Ailes)
+            Dim dedupe As String = DuplicateRemover(stuff)
+            sortedAisles = Regex.Split(dedupe, ",")
+            For z = 0 To UBound(sortedAisles)
+                sortedAisles(z) = sortedAisles(z).PadLeft(2)
+            Next
+        Next
+        Array.Sort(sortedAisles)
+        Console.ReadLine()
+
+    End Sub
+
+    Sub CatSorter()
+        'Sort the categories in Alphabetical order
+        Dim Category(sizer) As String
+
+        For b = 0 To sizer
+            Category(b) = fullArray2(b, 2)
+
+            Dim stuff As String = String.Join(",", Category)
+            Dim dedupe As String = DuplicateRemover(stuff)
+            sortedcategories = Regex.Split(dedupe, ",")
+
+        Next
+        Try
+            Array.Sort(sortedcategories)
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+
+    Public Sub CategoryButton_CheckedChanged(sender As Object, e As EventArgs) Handles CategoryButton.CheckedChanged
+        'Updates the combobox between Aisle numbers or the Category of the Item  
+
+        If CategoryButton.Checked = True Then
+            SelectItemComboBox.Items.Clear()
+            SelectItemComboBox.Items.Add("Show All")
+            For p = 0 To UBound(sortedcategories)
+                SelectItemComboBox.Items.Add(sortedcategories(p))
+            Next
+            SelectItemComboBox.SelectedItem = "Show All"
+        Else
+            SelectItemComboBox.Items.Clear()
+            SelectItemComboBox.Items.Add("Show All")
+            For d = 0 To UBound(sortedAisles)
+                SelectItemComboBox.Items.Add(sortedAisles(d))
+            Next
+            SelectItemComboBox.SelectedItem = "Show All"
+        End If
+
+    End Sub
+
+    Function DuplicateRemover(ByVal sInput As String, Optional ByVal sDelimiter As String = ",") As String
+
+        'Removes Duplicate Values in an array.
+        Dim varSection As String
+        Dim sTemp As String
+
+        For Each varSection In Split(sInput, sDelimiter)
+            If InStr(1, sDelimiter & sTemp & sDelimiter, sDelimiter & varSection & sDelimiter, vbTextCompare) = 0 Then
+                sTemp = sTemp & sDelimiter & varSection
+            End If
+        Next varSection
+
+        DuplicateRemover = Mid(sTemp, Len(sDelimiter) + 1)
+
+    End Function
+
+    Private Sub SelectItemComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles SelectItemComboBox.SelectedIndexChanged
+        Dim anArray(1) As String
+        SelectItemComboBox.SelectedItem.ToString()
+        DisplayListBox.Items.Clear()
+
+        'Sort items into into Displaybox based off of aisle or category.
+        For s = 0 To sizer
+            For f = 0 To 2
+                If AisleRadioButton.Checked = True Then
+                    If SelectItemComboBox.SelectedItem.ToString() = fullArray2(s, f) Or SelectItemComboBox.SelectedItem.ToString() = " " & fullArray2(s, f) Then
+                        DisplayListBox.Items.Add(fullArray2(s, 0))
+                    End If
+                Else
+                    If SelectItemComboBox.SelectedItem.ToString() = fullArray2(s, f) Then
+                        DisplayListBox.Items.Add(fullArray2(s, 0))
+                    End If
+                End If
+
+            Next
+            If SelectItemComboBox.SelectedItem.ToString() = "Show All" Then
+                Try
+                    DisplayListBox.Items.Add(fullArray2(s, 0))
+                Catch ex As Exception
+                End Try
+            End If
+        Next
+    End Sub
+
+    Private Sub DisplayListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DisplayListBox.SelectedIndexChanged
+
+        'Updates the display label with the location when an item is selected.
+        For x = 0 To sizer
+            If DisplayListBox.SelectedItem.ToString() = fullArray2(x, 0) Then
+                DisplayLabel.Text = ("You will find " & fullArray2(x, 0) & " in aisle " & fullArray2(x, 1) & " around the " & fullArray2(x, 2))
+            End If
+        Next
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub AboutTopMenuItem_Click(sender As Object, e As EventArgs) Handles AboutTopMenuItem.Click
+        AboutForm.Show()
+    End Sub
 End Class
